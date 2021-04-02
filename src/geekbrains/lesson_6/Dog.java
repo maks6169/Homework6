7450500ffbedb07e1277d74a45a7ff39ef27b9bf
maks6169 <maks6169@rambler.ru>
2021-04-02 22:04:09 +0300

    Lesson6

--- src/geekbrains/lesson_6/Dog.java
+++ src/geekbrains/lesson_6/Dog.java
@@ -1,4 +1,26 @@
 package geekbrains.lesson_6;
 
-public class Dog {
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run(int runLength) {
+        if (runLength <= 500) {
+            super.run(runLength);
+        } else {
+            System.out.printf("Собака %s  не может больше бежать\n", name);
+        }
+    }
+
+    @Override
+    public void swim(int swimLength) {
+        if (swimLength <= 10) {
+            super.swim(swimLength);
+        } else {
+            System.out.printf("Собака %s больше не может плыть\n", name);
+        }
+    }
 }
+
--- src/geekbrains/lesson_6/Main.java
+++ src/geekbrains/lesson_6/Main.java
@@ -3,6 +3,16 @@ package geekbrains.lesson_6;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal[] zoo = {
+                new Cat("Barsik"),
+                new Cat("Murzik"),
+                new Dog("Bobik"),
+                new Dog("Barbos")};
+        for (Animal animal : zoo) {
+            animal.swim(10);
+            animal.run(300);
+        }
     }
 }
+
